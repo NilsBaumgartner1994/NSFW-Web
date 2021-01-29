4efc5925fcb7f62733d3a01ecca0c0093d5db226
Nils <nilsbaumgartner1994@gmail.com>
2021-01-29 13:50:01 +0100

    changed webpack target

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    target: 'node',
+    target: 'web',
     externals: [
         nodeExternals({modulesFromFile: true,})
     ],
