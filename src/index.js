893a8d0bade8489939521bbc7e26ea1546428e1b
Nils <nilsbaumgartner1994@gmail.com>
2021-01-29 14:05:04 +0100

    Added more exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // this should be the entry point to your library
 module.exports = {
     ServerWeb: require('./ServerWeb').default, // eslint-disable-line global-require
+    App: require('./App').default, // eslint-disable-line global-require
+
 };
