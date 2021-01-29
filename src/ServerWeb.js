3a08b46954746ec4666b77f6d280aba61c5b799c
Nils <nilsbaumgartner1994@gmail.com>
2021-01-29 14:00:23 +0100

    Wrapped as Component

--- src/ServerWeb.js
+++ src/ServerWeb.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import ScrollToTop from './showcase/scrolltotop/ScrollToTop';
@@ -8,26 +8,24 @@ import {ReactComponentToHTMLImageRenderer} from "./helper/ReactComponentToHTMLIm
 
 document.title = config.title;
 
-export default class ServerWeb {
+export default class ServerWeb extends Component{
 
-    start(){
-        let port = window.location.port;
-        if(!!port && port===""+3000){
-            let newAddress = window.location.protocol+'//'+window.location.hostname;
-            window.location.replace(newAddress)
-        } else {
-            ReactDOM.render(
-                <div>
-                    <Router>
-                        <ScrollToTop>
-                            <App></App>
-                        </ScrollToTop>
-                    </Router>
-                    {ReactComponentToHTMLImageRenderer.getHiddenContainer()}
-                </div>,
-                document.getElementById('root')
-            );
-        }
+    constructor(props) {
+        super(props);
     }
+
+    render(){
+        return(
+            <div>
+                <Router>
+                    <ScrollToTop>
+                        <App></App>
+                    </ScrollToTop>
+                </Router>
+                {ReactComponentToHTMLImageRenderer.getHiddenContainer()}
+            </div>
+        )
+    }
+
 }
 
