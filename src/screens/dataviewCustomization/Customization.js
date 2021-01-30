baa7d0744ebd55b190546ca09f076300917cf518
Nils <nilsbaumgartner1994@gmail.com>
2021-01-30 20:40:46 +0100

    added debug output

--- src/screens/dataviewCustomization/Customization.js
+++ src/screens/dataviewCustomization/Customization.js
@@ -13,8 +13,11 @@ export class Customization extends Component {
         let tableName = parentState.tableName;
 
         let customComponentForTablename = App.DATAVIEW_CUSTOMIZATIONS[tableName];
+        console.log("Render Customization for: "+tableName);
 
         if(!!customComponentForTablename){
+            console.log("Custom Component found, lets rock");
+            console.log(customComponentForTablename);
             return <customComponentForTablename reloadPage={this.props.reloadPage} instance={instance} parentState={this.props.parentState} parentProps={this.props.parentProps} />
         }
         return null;
